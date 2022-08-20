23c78889562bbbaa73c73ea5f80a2b698b220499
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-20 17:52:32 +0300

    commit tree 04c04e8096ef5f329fc75173fa6dffb832065d2b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+    def name= (dogs_name)
+        @this_dogs_name = dogs_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+bosco = Dog.new
+bosco.name = "Bosco"
+
+puts bosco.name
